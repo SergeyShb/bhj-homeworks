6dc17eeb05066b6770ba683932718092622fbefc
Ser <Shabaykin.S@gmail.com>
2023-05-17 21:17:36 +0300

    Задание выполнено

---
+++ element-search/slider/task.js
@@ -0,0 +1,27 @@
+let sliderArrowPrev = document.querySelector(".slider__arrow_prev");
+let sliderArrowNext = document.querySelector(".slider__arrow_next");
+let allSliderItem = Array.from(document.querySelectorAll(".slider__item"));
+
+let slidIndex = 0;
+
+
+sliderArrowNext.onclick = function() {
+    allSliderItem[slidIndex].classList.remove("slider__item_active");
+    if (slidIndex === allSliderItem.length - 1) {
+        slidIndex = 0; 
+    } else {
+        slidIndex++;
+    }
+    allSliderItem[slidIndex].classList.add("slider__item_active");
+};
+
+
+sliderArrowPrev.onclick = function() {
+    allSliderItem[slidIndex].classList.remove("slider__item_active");
+    if (slidIndex === 0) {
+        slidIndex = allSliderItem.length - 1; 
+    } else {
+        slidIndex--;
+    }
+    allSliderItem[slidIndex].classList.add("slider__item_active");
+};
